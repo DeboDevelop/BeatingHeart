1e7abc8d7fd2841bbeb82fc90aaffc2669bf0a56
DeboDevelop <debajyotidofficial@gmail.com>
2020-08-02 18:54:00 +0530

    Added Url checking functionality with request

--- src/checker/cron.py
+++ src/checker/cron.py
@@ -1,6 +1,18 @@
+import requests
+from .models import Website
+
 def check_and_mail():
-    # your functionality goes here
-    print("Cron is Working")
-    f = open('/home/tukai/Desktop/cron.txt', "w+")
-    f.close()
+    all_entry = Website.objects.all()
+    for entry in all_entry:
+        try:
+            r = requests.head(entry.url)
+            if r.status_code == 200:
+                print("Website is up: ")
+                print(entry.url)
+            else:
+                print("Website is down")
+                print(entry.url)
+        except:
+            print("Website is down")
+            print(entry.url)
     return true
