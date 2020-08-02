e9abdd62693d5df28796482fbe578f004e1e54a0
DeboDevelop <debajyotidofficial@gmail.com>
2020-08-02 17:54:43 +0530

    Created the Website model and register it in admin panel

--- src/checker/models.py
+++ src/checker/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Website(models.Model):
+    url = models.URLField(unique=True)
+    email = models.EmailField()
+    currently_up = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.url
