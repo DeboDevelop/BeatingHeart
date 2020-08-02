e69566c9e8a4b85f8000fd22f5d29977e380c052
DeboDevelop <debajyotidofficial@gmail.com>
2020-08-02 18:24:31 +0530

    Creted Crontab to schedule our task

--- src/BeatingHeart/settings.py
+++ src/BeatingHeart/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_crontab',
     'checker'
 ]
 
@@ -119,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRONJOBS = [
+    ('*/2 * * * *', 'checker.cron.check_and_mail', '>> /home/tukai/Desktop/file.log')
+]
---
+++ src/checker/cron.py
@@ -0,0 +1,6 @@
+def check_and_mail():
+    # your functionality goes here
+    print("Cron is Working")
+    f = open('/home/tukai/Desktop/cron.txt', "w+")
+    f.close()
+    return true
