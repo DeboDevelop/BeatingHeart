8dcf7c8d71eaeb1c70a5ca92637b8ebe7a809511
DeboDevelop <debajyotidofficial@gmail.com>
2020-08-02 20:16:41 +0530

    Made the homepage redirect to /admin page

--- src/checker/views.py
+++ src/checker/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import redirect
 
 def index(request):
-    return HttpResponse("<h1>Home Page</h1>")
+    return redirect('/admin')
